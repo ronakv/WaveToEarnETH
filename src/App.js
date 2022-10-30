f38dd4bb68e0326d04ef377af65826751c98c9e4
Ronak Vimadalal <ronakvimadalal@live.com>
2022-10-29 21:48:37 -0700

    Added function to Wave

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import { ethers } from "ethers";
 import './App.css';
+import abi from './utils/WavePortal.json';
 
 const getEthereumObject = () => window.ethereum;
 const findMetaMaskAccount = async () =>{
@@ -28,9 +29,9 @@ const findMetaMaskAccount = async () =>{
 }
 export default function App() {
   const [currentAccount, setCurrentAccount] = useState("");
-  const wave = () => {
-    
-  }
+  const contractAddress = "0x588E2e5459094938fd133B4Cab7079a068CD7Bb9";
+  const contractABI = abi.abi;
+
   const connectWallet = async () =>{
     try{
       const ethereum = getEthereumObject();
@@ -48,6 +49,36 @@ export default function App() {
     catch(error){
       console.error(error);
     }
+  }
+
+  const wave = async () =>{
+    try{
+      const { ethereum } = window;
+      if(ethereum){
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signers);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved Total Wave Count: ", count.toNumber());
+
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining....", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined ---", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        console.log("New count is :", count.toNumber());
+      }
+      else{
+        console.log("Ethereum object does not exist");
+      }
+    }
+    catch(error){
+      console.error(error)
+    }
+
   }
   useEffect(async ()=>{
     const ethereum = getEthereumObject();
