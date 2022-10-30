4b21d10ff844e1da04e84f0af251fb8007b26798
Ronak Vimadalal <ronakvimadalal@live.com>
2022-10-29 20:05:10 -0700

    Ethereum check

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import * as React from "react";
+import React, {useEffect} from "react";
 import { ethers } from "ethers";
 import './App.css';
+
+const getEthereumObject = () => window.ethereum;
 
 export default function App() {
 
   const wave = () => {
     
   }
-  
+  useEffect(()=>{
+    const ethereum = getEthereumObject();
+    if(!ethereum){
+      console.log('Are you logged onto metamask?');
+    }
+    else{
+      console.log('Ethereum object is: ', ethereum);
+    }
+  }, []);
+
   return (
     <div className="mainContainer">
 
@@ -17,7 +28,7 @@ export default function App() {
         </div>
 
         <div className="bio">
-        I am Ronak and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        I am Ronak and I work as an engineer that's pretty cool right? Connect your Ethereum wallet and wave at me!
         </div>
 
         <button className="waveButton" onClick={wave}>
